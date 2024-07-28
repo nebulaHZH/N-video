4a123fa9b81ee2cb76da8eb977411ecf7199e860
username <3113613254@qq.com>
2024-07-28 21:01:22 +0800

    add front

--- nfront1/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "element-plus/dist/index.css";
-import ElementPlus from "element-plus";
-const app= createApp(App)
-app.use(router).use(ElementPlus)
-app.mount("#app");
